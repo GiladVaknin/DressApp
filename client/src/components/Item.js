314061242169f668125f463b071dcbe4499f4121
IlayTissona <ilay211@gmail.com>
2021-07-05 17:41:13 +0300

    lazy-eager-loading

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -4,21 +4,21 @@ const axios = require("axios");
 function Item(props) {
   const [item, setItem] = useState(props.item);
 
-  //   useEffect(() => {
-  //     getPreview(props.item).then((res) => setItem(res));
-  //   }, [props.item]);
+  useEffect(() => {
+    getPreview(props.item).then((res) => setItem(res));
+  }, [props.item]);
 
-  //   function getPreview(item) {
-  //     return axios({
-  //       method: "POST",
-  //       url: "http://localhost:8080/api/preview",
-  //       data: {
-  //         query: item,
-  //       },
-  //     })
-  //       .then((res) => res.data)
-  //       .catch(console.log);
-  //   }
+  function getPreview(item) {
+    return axios({
+      method: "POST",
+      url: "http://localhost:8080/api/preview",
+      data: {
+        query: item,
+      },
+    })
+      .then((res) => res.data)
+      .catch(console.log);
+  }
 
   function getPrevPrice() {
     if (item.prevPrice > 0)
@@ -30,23 +30,20 @@ function Item(props) {
       );
     else return;
   }
-  //  const item = getPreview(props.item);
 
-  return (
-    item && (
-      <div class="item">
-        <img src={item.imgSrc} alt={item.title} />
-        <h2 className="itemTitle">{item.title}</h2>
-        <span className="priceDetails">
-          <h3 className="price">{item.price} ₪</h3>
-          {getPrevPrice()}
-        </span>
-        <h2>
-          <a href={item.linkToBuy}>Shop Now!</a>
-        </h2>
-      </div>
-    )
-  );
+  return item ? (
+    <div class="item">
+      <img src={item.imgSrc} alt={item.title} />
+      <h2 className="itemTitle">{item.title}</h2>
+      <span className="priceDetails">
+        <h3 className="price">{item.price} ₪</h3>
+        {getPrevPrice()}
+      </span>
+      <h2>
+        <a href={item.linkToBuy}>Shop Now!</a>
+      </h2>
+    </div>
+  ) : null;
 }
 
 export default Item;
