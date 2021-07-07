4ccb4661e9dbcc2122144c875870590be17636a7
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-07-07 11:31:45 +0300

    Get recent function added

--- client/src/App.js
+++ client/src/App.js
@@ -6,51 +6,8 @@ import { useState, useEffect } from "react";
 const axios = require("axios");
 
 function App() {
-  const exc = [
-    {
-      storeName: "Asos",
-      linkToBuy:
-        "https://www.asos.com/south-beach/south-beach-scrunch-exaggerated-bikini-set-in-orange/grp/203937?colourwayid=60424450&cid=27428#22770569",
-    },
-    {
-      storeName: "Asos",
-      linkToBuy:
-        "https://www.asos.com/topshop/topshop-shirred-crop-bikini-top-in-purple/prd/200402846?colourwayid=200402850&cid=2238",
-    },
-    {
-      storeName: "Asos",
-      linkToBuy:
-        "https://www.asos.com/south-beach/south-beach-scrunch-exaggerated-bikini-set-in-orange/grp/203937?colourwayid=60424450&cid=27428#22770569",
-    },
-    {
-      storeName: "Asos",
-      linkToBuy:
-        "https://www.asos.com/south-beach/south-beach-scrunch-exaggerated-bikini-set-in-orange/grp/203937?colourwayid=60424450&cid=27428#22770569",
-    },
-    {
-      storeName: "Asos",
-      linkToBuy:
-        "https://www.asos.com/asos-design/asos-design-textured-beach-shorts-and-shirt-in-white/grp/33061?colourwayid=60306022&cid=2238#21876229",
-    },
-    {
-      storeName: "Asos",
-      linkToBuy:
-        "https://www.asos.com/reclaimed-vintage/reclaimed-vintage-inspired-recycled-underwired-bikini-top-in-snake-print/prd/23193742?colourwayid=60458087&cid=2238",
-    },
-  ];
-
   const [items, setItems] = useState([]);
   const [shownItems, setShownItems] = useState([]);
-
-  // useEffect(() => {
-  //   getItems({
-  //     gender: "women",
-  //     category: "Clothing",
-  //     productType: "Tops",
-  //     colors: ["Black"],
-  //     sizes: ["S"],
-  //   }).then((res) => setItems(res.data));
-  // }, []);
 
   function getItems(query) {
     return axios({
@@ -99,6 +56,19 @@ function App() {
     });
   }
 
+  function getRecent() {
+    return axios({
+      method: "GET",
+      url: "http://localhost:8080/api/recent",
+    });
+  }
+
+  useEffect(() => {
+    getRecent()
+      .then((res) => setItems(res.data))
+      .catch(console.log);
+  }, []);
+
   return (
     <div className="App">
       <div id="header"></div>
@@ -108,7 +78,7 @@ function App() {
           return <Item item={item} />;
         })}
       </div>
-      <button onClick={loadItems}>LOAD MORE</button>
+      {items.length ? <button onClick={loadItems}>LOAD MORE</button> : null}
     </div>
   );
 }
