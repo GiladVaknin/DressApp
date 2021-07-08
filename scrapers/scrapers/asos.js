52bd52387618ceee7393d9f6d03b76ef22a115e4
IlayTissona <ilay211@gmail.com>
2021-07-08 14:59:35 +0300

    asos size bug

--- scrapers/scrapers/asos.js
+++ scrapers/scrapers/asos.js
@@ -1,8 +1,13 @@
 const puppeteer = require("puppeteer");
 const BASE_URL = "https://www.asos.com";
-const axios = require("axios");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+
+const SIZE_VALUES = {
+  XS: "5188",
+  S: "4430",
+  M: "4418",
+  L: "5164",
+  XL: "5176",
+};
 
 async function main(product) {
   const headless = process.env.headless || false;
@@ -32,37 +37,41 @@ async function main(product) {
     const productType = await openMenu.$(`div[title='${product.productType}']`);
 
     productType && (await productType.evaluate((e) => e.click()));
+    if (product.colors?.length) {
+      await page.waitForXPath(`//button[contains(., 'Colour')]`);
+      const [colour] = await page.$x(`//button[contains(., 'Colour')]`);
 
-    await page.waitForXPath(`//button[contains(., 'Colour')]`);
-    const [colour] = await page.$x(`//button[contains(., 'Colour')]`);
-
-    if (colour) {
-      await colour.click();
-      await page.setViewport({ width: 770, height: 1300 });
-      for (const color of product.colors) {
-        await page.waitForSelector(`li._3LB03xF`);
-        const [colorBtn] = await page.$x(`//label[contains(., "${color}")]`);
-        await colorBtn.evaluate((node) => node.scrollIntoView());
-        await colorBtn.click();
-        // await page.waitForNavigation({ waitUntil: "networkidle2" });
+      if (colour) {
+        await colour.click();
+        await page.setViewport({ width: 770, height: 1300 });
+        for (const color of product.colors) {
+          await page.waitForSelector(`li._3LB03xF`);
+          const [colorBtn] = await page.$x(`//label[contains(., "${color}")]`);
+          await colorBtn.evaluate((node) => node.scrollIntoView());
+          await colorBtn.click();
+        }
       }
     }
+    if (product.sizes?.length) {
+      await page.waitForNavigation();
+      const [size] = await page.$x(`//button[.='Size']`);
 
-    await page.waitForXPath(`//button[contains(., 'Size')]`);
-    const [size] = await page.$x(`//button[contains(., 'Size')]`);
+      if (size) {
+        await size.click();
 
-    if (size) {
-      await size.evaluate((node) => node.click());
-
-      for (const size of product.sizes) {
-        await page.waitForSelector(`li._3LB03xF`);
-        const [sizeSelect] = await page.$x(`//label[contains(., '${size}')]`);
-        await sizeSelect.evaluate((node) => node.scrollIntoView());
-        sizeSelect && (await sizeSelect.click());
-        // await page.waitForNavigation({ waitUntil: "networkidle2" });
+        for (const size of product.sizes) {
+          const sizeValue = SIZE_VALUES[size];
+          await page.waitForSelector(`li._3LB03xF div`);
+          const sizeSelect = await page.$(`input[value='${sizeValue}']`);
+          console.log(sizeSelect?._remoteObject.description);
+          // sizeSelect &&
+          //   (await sizeSelect.evaluate((node) => node.scrollIntoView()));
+          sizeSelect &&
+            (await sizeSelect.evaluate(async (node) => await node.click()));
+        }
       }
     }
-
+    await page.waitForSelector("._2qG85dG");
     const items = await page.$$("._2qG85dG");
 
     const allItemsFormatted = await Promise.all(
@@ -86,93 +95,6 @@ async function main(product) {
     console.log(err);
     browser.close();
   }
-}
-
-async function getItem(itemElem) {
-  const item = { storeName: "Asos" };
-
-  item.title = await itemElem
-    .$("._3J74XsK")
-    .then((elem) => elem.getProperty("innerText"))
-    .then((handle) => handle.jsonValue());
-
-  item.price = await itemElem
-    .$("._16nzq18")
-    .then((elem) => elem.getProperty("innerText"))
-    .then(async (handle) => {
-      const priceWithShach = await handle.jsonValue();
-      return Number(priceWithShach.replace("ILS", ""));
-    });
-
-  item.linkToBuy = await itemElem
-    .$("a._3TqU78D")
-    .then((elem) => elem.getProperty("href"))
-    .then((handle) => handle.jsonValue());
-
-  item.rank = await getRank(item.linkToBuy);
-  item.imgSrc = await getImgSrc(item.linkToBuy);
-
-  return item;
-}
-
-async function getRank(itemLink, secondTry) {
-  var data = "";
-
-  var config = {
-    method: "get",
-    url: itemLink,
-    headers: {
-      Cookie:
-        "_abck=BC3E379FA5195C118296920B40183288~-1~YAAQpJf2SMX2RBh6AQAAUhoyJgbZcFl+WzwhJrd69PV4orD1wVaHJxwAWQMnYf3oqRYY7McmqwxJefioyrGiH5uSRNpXuw72xKPMQ4ox4iWriGdaVA6jU3skElkFGKiNkAn6fP1a9jF6ZuqO0erpn83SfBcwv8cjlnjlC6a7Tale+SVC6pCdHRzE12FK6ZlyQc48PUcjcQuNg1kbKyhD/FU9M8mGWpy8C66hSl5loSeepeh1nOri2DG58vfHvCcxCSuLCArzDb8CwWL06ecxyJ5hRfm7VPKFH15CjKxoRtSVpUR8hDikTv3lfqSxSRGOxKFIVCJhNFG9VlIEty0f6mZIWe2anHHvEnXhLtHXqMSjydajeJpEunVmVS+m1KRCIG8vBxc=~-1~-1~-1; bm_sz=734C3652FA6965C69CA1A67BEA4CB858~YAAQpJf2SMT2RBh6AQAAUhoyJgzj8kKHV0QX/q0/jFdkOpbe5Ai2xgSopgHtg8eS9akotlg/g7PXmtfx04bbk57YshzcgLn3pC5NrdviZ4Lp+HoeTsexVT/bArA8/xz6COLsLZ1WuvMI3rAmh1sihOIQ4sNv9BgM9RGlGinXso7YGVUii2fnzH34O4PcPw==; geocountry=IL",
-    },
-    data: data,
-  };
-
-  return axios(config)
-    .then(async function (response) {
-      const data = JSON.stringify(response.data);
-      const t = data.indexOf(`averageOverallRating`);
-      const d = data.slice(t + 23, t + 26);
-      const needFix = d.indexOf(",");
-      const ut = d.indexOf("ut");
-
-      if (ut !== -1) {
-        return secondTry ? null : await getRank(itemLink, true);
-      }
-      if (needFix !== -1) {
-        return d.slice(0, needFix);
-      }
-      return d;
-    })
-    .catch(function (error) {
-      console.log("error in RANK");
-    });
-}
-
-async function getImgSrc(itemLink) {
-  var data = "";
-
-  var config = {
-    method: "get",
-    url: itemLink,
-    headers: {
-      Cookie:
-        "_abck=BC3E379FA5195C118296920B40183288~-1~YAAQpZf2SFB3tSd6AQAAi4x7Kgai7Ea4cVHwM0ZjhScj0fjlm5ZaE70yXbXKUSZnKp8w0bebwEw/w20KmgKhkNkf97RFdouAuhu2tsGghGISyAyFLCB3aUhIPCeSU7PD/ZuWoDXwtE0/tV41toZIOwar/9KyLo0JYt59+I8FSMdwnsiEFgMuqTm0HTBPUcv/vutW7tgvMXR8IGE+15kKv1EObAVS47VTrsWpiXDdqhyFLOmFyCm8iFWbofk/MOmimg7GTxkMnt///mVxnrDcl9TSavb+UXfFMNAouXFgKIOHQ+aEuMbWu/8ioSLZkrFM0dKLZ39JuMJ/xVpA5OeUOLzg1B3tIIAhSxxQil0Z/EjEPKmfdGE6k9K0DEfGdgheo5I5lZc=~-1~-1~-1; bm_sz=82DA8D51AB0F00505437788642B6D38C~YAAQpZf2SE93tSd6AQAAi4x7Kgy9S7c4694gfpWNQ9XF63/SJas3uRgz9IORrCNRWqKJ4jNXl+jzOrTOeWxIM86ucCIJZcrvIgxwdUPNI36Ij1dMe+DwhmOZl1lCocEaRMv74QjaWFwGLppuoWC9WkNsgtOrrymWeHYARpGPnTiVarWuh6WJdRziSL+n8w==; geocountry=IL",
-    },
-    data: data,
-  };
-
-  return axios(config)
-    .then(async function (response) {
-      const { window } = await new JSDOM(response.data, {
-        runScripts: "dangerously",
-      });
-
-      return window.window.asos.pdp.config.product.images[0].url;
-    })
-    .catch(function (error) {
-      console.log("error in imgSRC");
-    });
 }
 
 module.exports = main;
