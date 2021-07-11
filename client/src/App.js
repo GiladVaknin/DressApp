bddba4f2d4358d9eae853b0e9b25f2a4580fa5b4
IlayTissona <ilay211@gmail.com>
2021-07-11 15:56:17 +0300

    little styling

--- client/src/App.js
+++ client/src/App.js
@@ -74,7 +74,7 @@ function App() {
     <div className="App">
       <div id="header"></div>
       <FilterPannel getItems={getItems} />
-      <div id="mobileToolTip">{`<<`}Swipe left for details </div>
+      <div id="mobileToolTip">{`<<`} Swipe left for details </div>
       {shownItems.length ? (
         <div className="items">
           {shownItems.map((item) => {
